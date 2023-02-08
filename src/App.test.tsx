8a0a775c0338e993f755343bb721cff74eeb0a5c
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-02-08 10:23:42 +0200

    add test if header renders correctly

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component", () => {
+  it("renders header", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/SWAPI - The Star Wars API/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
