1244a5516e26174790c104690f6db841223642b6
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-03-30 09:38:49 +0300

    minor improving - emphasise name

--- src/components/StarWarsCharacter.tsx
+++ src/components/StarWarsCharacter.tsx
@@ -1,5 +1,7 @@
 export type Character = { name: string };
 
 export const StarWarsCharacter: React.FC<Character> = ({ name }) => (
-  <div> Person from the Star War People {name}</div>
+  <div>
+    Person from the Star War People - <em>{name}</em>
+  </div>
 );
